6f9fccc821c87808927a332449bba452591b79b8
asm-dev <salamanca.m@outlook.com>
2022-10-20 18:44:45 +0200

    Append()

--- script.js
+++ script.js
@@ -1,5 +1,20 @@
-array = [" 1"," 2"," 3"," 4"," 5"]
+array = [1,2,3,4,5]
+
+function updateArray() {
+    document.getElementById("new-list").innerHTML = array;
+}
 
 function reverseList() {
-    document.getElementById("new-list").innerHTML = "[" + array.reverse() + "]";
+    array.reverse();
+    updateArray()
+}
+
+function appendElement() {
+    array.push(prompt("Please introduce the element you would like to append: "));
+    updateArray()
+}
+
+function popElement() {
+    array.push(prompt("Please introduce the element index: "));
+    updateArray()
 }
