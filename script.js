c3dee7391f24c351780fe45be165cb970aa03093
asm-dev <salamanca.m@outlook.com>
2022-10-20 19:49:35 +0200

    insert() and sort()

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-originalArray = [1,2,3,4,5]
 array = [1,2,3,4,5]
 
 function updateArray() {
@@ -7,6 +6,11 @@ function updateArray() {
 
 function reverseList() {
     array.reverse();
+    updateArray()
+}
+
+function sortList() {
+    array.sort();
     updateArray()
 }
 
@@ -21,31 +25,33 @@ function popElement() {
     updateArray()
 }
 
+function insertElement() {
+    element = prompt("Please introduce the element you would like to append: ");
+    index = prompt("Please introduce the element index: ")
+    array.splice(index, 0, element);
+    updateArray()
+}
+
 function clearList() {
     document.getElementById("new-list").innerHTML = "You have cleared the list. Meaning that the list is emtpy.";
     buttons = [...document.querySelectorAll(".list-method-button")];
-    buttons.forEach(function(x){
-        x.style.visibility = "hidden";
-    });
+    buttons.forEach(function(x){x.style.visibility = "hidden";});
     retrieveButtons = [...document.querySelectorAll(".retrieve-list")];
-    retrieveButtons.forEach(function(x){
-        x.style.visibility = "visible";
-    });
+    retrieveButtons.forEach(function(x){x.style.visibility = "visible";});
 }
 
 function retrieveList() {
     buttons = [...document.querySelectorAll(".list-method-button")];
-    buttons.forEach(function(x){
-        x.style.visibility = "visible";
-    });
+    buttons.forEach(function(x){x.style.visibility = "visible";});
     updateArray();
     retrieveButtons = [...document.querySelectorAll(".retrieve-list")];
-    retrieveButtons.forEach(function(x){
-        x.style.visibility = "hidden";
-    });
+    retrieveButtons.forEach(function(x){x.style.visibility = "hidden";});
 }
 
 function retrieveOriginalList() {
-    array = originalArray
+    array = [1,2,3,4,5];
     retrieveList()
 }
+
+// write function to show lenght, items with index
+
