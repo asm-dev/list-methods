1c6a670d47ff6d49b5791c59b598d076f3a8b7eb
asm-dev <salamanca.m@outlook.com>
2022-10-20 19:19:58 +0200

    pop() and clear()

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+originalArray = [1,2,3,4,5]
 array = [1,2,3,4,5]
 
 function updateArray() {
@@ -15,6 +16,36 @@ function appendElement() {
 }
 
 function popElement() {
-    array.push(prompt("Please introduce the element index: "));
+    index = prompt("Please introduce the element index: ")
+    array.splice(index, 1);
     updateArray()
+}
+
+function clearList() {
+    document.getElementById("new-list").innerHTML = "You have cleared the list. Meaning that the list is emtpy.";
+    buttons = [...document.querySelectorAll(".list-method-button")];
+    buttons.forEach(function(x){
+        x.style.visibility = "hidden";
+    });
+    retrieveButtons = [...document.querySelectorAll(".retrieve-list")];
+    retrieveButtons.forEach(function(x){
+        x.style.visibility = "visible";
+    });
+}
+
+function retrieveList() {
+    buttons = [...document.querySelectorAll(".list-method-button")];
+    buttons.forEach(function(x){
+        x.style.visibility = "visible";
+    });
+    updateArray();
+    retrieveButtons = [...document.querySelectorAll(".retrieve-list")];
+    retrieveButtons.forEach(function(x){
+        x.style.visibility = "hidden";
+    });
+}
+
+function retrieveOriginalList() {
+    array = originalArray
+    retrieveList()
 }
