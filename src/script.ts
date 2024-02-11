8094a2df45b251f66f4c877105e99e3c54850de9
asm-dev <salamanca.m@outlook.com>
2024-02-11 23:41:58 +0100

    chore: continue refactor work

--- src/script.ts
+++ src/script.ts
@@ -1,98 +1,114 @@
-//TODO: RandomMaths para crear emojis. El punto y coma nos debería dar bastante igual
-const ORIGINAL_EMOJI_LIST: string[] = ["&#128516;","&#128525;","&#128151;","&#128515;","&#128507;"]
+const DEFAULT_EMOJI_LIST: string[] = [
+  "&#128516;",
+  "&#128525;",
+  "&#128151;",
+  "&#128515;",
+  "&#128507;",
+];
 
-let emojiList: string[] = [...ORIGINAL_EMOJI_LIST]
-let selectedIndex;
+const emojiSelect = document.getElementById("emojis") as HTMLSelectElement;
 
-const askForIndex = () => Number(prompt("Please introduce the element index: "))
+let emojiList: string[] = [...DEFAULT_EMOJI_LIST];
 
-// TODO: 
-const getEmoji = () => {
-    const emojiSelect = document.getElementById('emojis') as HTMLSelectElement;
-    const selectedEmoji = emojiSelect.options
-    //[emojis.selectedIndex]
-    //const emojiText = selectedEmoji.text;
-    //return emojiText
+let selectedEmoji: string = emojiSelect.options[emojiSelect.selectedIndex].text;
+
+// Get when a new emoji is selected
+emojiSelect.addEventListener("change", () => {
+  selectedEmoji = emojiSelect.options[emojiSelect.selectedIndex].text;
+});
+
+const askForIndex = () =>
+  Number(prompt("Please introduce the element index: "));
+
+function displayNotAvailable(): void {
+  alert(
+    "This button isn't working currently, please bear with us while we finish building it :)"
+  );
 }
 
-function displayNotAvailable() {
-    alert("This button isn't working currently, please bear with us while we finish building it :)")
+function updateArray(): void {
+  document.getElementById("new-list").innerHTML = emojiList.toString();
 }
 
-function updateArray() {
-    document.getElementById("new-list").innerHTML = emojiList.toString();
-}
-
-
-//TODO: SWITCH o OPP. Fan o mesmo... 1. Acción 2. Update
-
-function appendElement() {
-    emojiList.push(getEmoji());
-    updateArray()
-}
-
-function insertElement() {
-    emojiList.splice(askForIndex(), 0, getEmoji());
-    updateArray()
-}
-
-function popElement() {
+//TODO: enum instead of magic strings
+const listMethod = (method: string) => {
+  if (method === "append") {
+    emojiList.push(selectedEmoji);
+  }
+  if (method === "insert") {
+    emojiList.splice(askForIndex(), 0, selectedEmoji);
+  }
+  if (method === "pop") {
     emojiList.splice(askForIndex(), 1);
-    updateArray()
-}
-
-function reverseList() {
+  }
+  if (method === "reverse") {
     emojiList.reverse();
-    updateArray()
-}
-
-function sortList() {
+  }
+  if (method === "sort") {
     emojiList.sort();
-    updateArray()
-}
+  }
+  updateArray();
+};
 
 function clearList() {
-    document.getElementById("new-list").innerHTML = "You have cleared the list. The list is now emtpy.";
-    let buttons = [...document.querySelectorAll(".list-method-button")];
-    buttons.forEach(function(x){x.style.display = "none";});
-    let retrieveButtons = [...document.querySelectorAll(".retrieve-list")];
-    retrieveButtons.forEach(function(x){x.style.display = "inline";});
-    document.getElementById("new-list").style.letterSpacing = "normal";
+  document.getElementById("new-list").innerHTML =
+    "You have cleared the list. The list is now emtpy.";
+  let buttons = [...document.querySelectorAll(".list-method-button")];
+  buttons.forEach(function (x) {
+    x.style.display = "none";
+  });
+  let retrieveButtons = [...document.querySelectorAll(".retrieve-list")];
+  retrieveButtons.forEach(function (x) {
+    x.style.display = "inline";
+  });
+  document.getElementById("new-list").style.letterSpacing = "normal";
 }
-
 
 // ¿Que carallo estou intentando facer aquí?
 function retrieveList() {
-    let buttons = [...document.querySelectorAll(".list-method-button")];
-    buttons.forEach(function(x){x.style.display = "inline";});
-    updateArray();
-    let retrieveButtons = [...document.querySelectorAll(".retrieve-list")];
-    retrieveButtons.forEach(function(x){x.style.display = "none";});
-    document.getElementById("new-list").style.letterSpacing = "0.2em";
+  let buttons = [
+    ...document.querySelectorAll(".list-method-button"),
+  ] as HTMLElement[];
+  buttons.forEach((button) => (button.style.display = "inline"));
+
+  updateArray();
+  let retrieveButtons = [...document.querySelectorAll(".retrieve-list")];
+  retrieveButtons.forEach(function (x) {
+    x.style.display = "none";
+  });
+  document.getElementById("new-list").style.letterSpacing = "0.2em";
 }
 
 function retrieveOriginalList() {
-    emojiList = [...ORIGINAL_EMOJI_LIST];
-    retrieveList()
+  emojiList = [...DEFAULT_EMOJI_LIST];
+  retrieveList();
 }
 
-
 function showElements() {
-    detailsList = document.createElement("ul");
-    detailsList.classList.add("array-details-list-elements")
-    detailsList.style.listStyleType = "none";
-    document.body.appendChild(detailsList)
-    
-        array.forEach(function(x){
-            li = document.createElement("li");
-            detailsList.appendChild(li).innerHTML = "Index: <b>" + array.indexOf(x) + "</b> ---> " + x
-        });
-        let totalEmojis = array.length;
-        li = document.createElement("li");
-        detailsList.appendChild(li).innerHTML = "Number of items: " + totalEmojis;
-    }
+  detailsList = document.createElement("ul");
+  detailsList.classList.add("array-details-list-elements");
+  detailsList.style.listStyleType = "none";
+  document.body.appendChild(detailsList);
+
+  emojiList.forEach(function (x) {
+    li = document.createElement("li");
+    detailsList.appendChild(li).innerHTML =
+      "Index: <b>" + array.indexOf(x) + "</b> ---> " + x;
+  });
+  let totalEmojis = array.length;
+  li = document.createElement("li");
+  detailsList.appendChild(li).innerHTML = "Number of items: " + totalEmojis;
+}
 
 function removeListDetails() {
-    document.querySelectorAll(".array-details-list-elements").forEach(function(x){x.remove()})
-    document.querySelectorAll(".array-details-list-indexes").forEach(function(x){x.remove()})
+  document
+    .querySelectorAll(".array-details-list-elements")
+    .forEach(function (x) {
+      x.remove();
+    });
+  document
+    .querySelectorAll(".array-details-list-indexes")
+    .forEach(function (x) {
+      x.remove();
+    });
 }
